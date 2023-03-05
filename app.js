e13af49428af56d57cfd41619d9bbcfdb062ffc6
Aine Doyle <ainedoyleAD@gmail.com>
2023-03-05 13:30:10 +0000

    fixed some bugs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log("Hello World!");
 
 let bigString = "";
-let littleString = "Ans = 0";
+let littleString = "";
 let evalString = "";
+let calcuated = false;
 
 const equation = document.querySelector(".equation");
 const result = document.querySelector(".result");
@@ -10,7 +11,7 @@ const result = document.querySelector(".result");
 (() => {
   console.log("Running!");
   bigString = "";
-  littleString = "Ans = 0";
+  littleString = "";
 
   result.innerHTML = bigString ? bigString : "0";
   equation.innerHTML = littleString;
@@ -23,33 +24,38 @@ const updateEquationElement = () => {
   equation.innerHTML = littleString;
 }
 
+const back = () => {
+  if (!bigString || !evalString) return;
+  bigString = bigString.split(" ").slice(0, -1).join(" ");
+  evalString = evalString.split(" ").slice(0, -1).join(" ");
+  calcuated=false;
+
+  if (!bigString) {
+    clear();
+  } else {
+    updateResultElement();
+  }
+}
+
 const clear = () => {
   bigString = "";
-  littleString = "Ans = 0";
+  littleString = "";
   evalString = "";
+  calcuated=false;
 
   updateResultElement();
   updateEquationElement();
 }
 
 const evaluate = () => {
-  console.log("got here", evalString)
   const res = eval(evalString);
   littleString = bigString;
-  bigString = res;
+  bigString = res.toString();
+  evalString = res.toString();
+  calcuated=true;
 
   updateResultElement();
   updateEquationElement();
-}
-
-// e.target.childNodes[0].data;
-
-const calculateNum = (eq) => {
-  console.log("calculating...");
-}
-
-const pressedEqualHandler = () => {
-  console.log("pressed equals button!");
 }
 
 const btns = document.querySelectorAll(".btn");
@@ -58,6 +64,13 @@ btns.forEach(btn => btn.addEventListener("click", () => {
   if (!bigString) {
     bigString = btn.value;
     evalString = btn.value;
+    calcuated = false;
+  } else if (calcuated) {
+    bigString = btn.value;
+    evalString = btn.value;
+    littleString = ""
+    calcuated=false;
+    updateEquationElement();
   } else {
     bigString += btn.value;
     evalString += btn.value;
@@ -73,13 +86,17 @@ evalBtns.forEach(btn => btn.addEventListener("click", (e) => {
 
   bigString += ` ${e.target.childNodes[0].data} `;
   evalString += btn.value;
-  
+
+  calcuated = false;
 
   updateResultElement();
 }));
 
+const backBtn = document.querySelector("#back");
+backBtn.addEventListener("click", back);
+
 const ACBtn = document.querySelector(".AC");
 ACBtn.addEventListener("click", clear);
 
-const equalBtn = document.querySelector(".blue");
-equalBtn.addEventListener("click", evaluate);
+const evaluateBtn = document.querySelector(".blue");
+evaluateBtn.addEventListener("click", evaluate);
